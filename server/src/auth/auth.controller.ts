9f630c03cda2800e2d46da94dab2e87560d0cf12
Gitlab CI <gitlab@ci.com>
2023-01-31 09:40:07 +0000

    Update server

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -9,24 +9,25 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  async login(@Body() body) {
+  async login(@Body() body: LoginDto) {
     return this.authService.login(body);
   }
 
   @Post('register')
-  async register(@Body() body) {
+  async register(@Body() body: LoginDto) {
     return this.authService.register(body);
   }
 
   @UseGuards(AuthGuard('jwt'))
   @Get('logout')
-  logout(@Req() req, @Res() res) {
+  logout(@Req() req: ParameterDecorator, @Res() res: ParameterDecorator) {
     return this.authService.logout(req, res);
   }
 }
